af20a270e33446d4307c0a3c83cb5e900538129f
Sergey Manzhos <opghld@gmail.com>
2023-03-28 18:52:02 +0300

    Update go-install.sh

--- go-install.sh
+++ go-install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo apt update && sudo apt upgrade -y
 sudo apt install make clang pkg-config libssl-dev libclang-dev build-essential git curl ntp jq llvm tmux htop screen unzip cmake -y
 cd /usr/src
